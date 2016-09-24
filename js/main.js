2d0b966d2ddaac26a9092a9cf9386f61ac8bb5b7
Shunsuke Toba <boushi_sukaimu@yahoo.co.jp>
2016-09-24 14:51:38 +0900

    js file

--- js/main.js
+++ js/main.js
@@ -11,27 +11,32 @@ jQuery(function() {
 
     canvas.createShape('circle1', 'circle', {x: 80, y: 160, r: 50}, {
       zindex: 15,
+      //図形のデフォルトのスタイル
       style: {
         strokeStyle: '#CCC',
           fillStyle: '#FF0',
           lineWidth: 2
         },
+        //ポインターが図形に乗ったときのスタイル
         hoverStyle: {
           strokeStyle: '#555',
           fillStyle: '#FF0',
           fadeSpeed: 300,
           easeType: 'easeInQuad'
         },
-          activeStyle: {
+        //図形をクリックした際のスタイル（ほとんど瞬間的）
+        activeStyle: {
           strokeStyle: '#00F',
           fillStyle: '#0FF'
         },
+        //図形の初期スタイルにグラデーション効果を付加
         gradient: {
           strokeStyle: '#F00',
           startFrom: 'center',
           color: ['#F00', '#FF0', 'rgba(0,255,255,0.8)'],
           position: [0, 0.5, 1]
         },
+        //クリックしたときの処理
         onClick: function () {
           this.animateShape('circle1', {
             endlinewidth: 5,
