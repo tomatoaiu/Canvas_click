6ff7fc2cea771fc3fbc312350fd3137eb5d66c2b
Shunsuke Toba <boushi_sukaimu@yahoo.co.jp>
2016-09-25 17:06:08 +0900

    js file
    点と点をクリックすると
    点の間の線の色を変更

--- js/main.js
+++ js/main.js
@@ -1,15 +1,20 @@
 $(function() {
     var canvas = $('#canvas');
 
+    var cClicked1 = false,
+        cClicked2 = false;
+
+    var dotName, target;
+
     //canvasサイズをブラウザ画面のサイズに
-    var can = $('#canvas').get(0);
+    var size = $('#canvas').get(0);
     var container = $('#wrap').get(0);
     sizing();
 
     //サイズ変更処理
     function sizing() {
-      can.height = container.offsetHeight;
-      can.width = container.offsetWidth;
+      size.height = container.offsetHeight;
+      size.width = container.offsetWidth;
     }
 
     //リサイズイベントの発生時
@@ -25,8 +30,20 @@ $(function() {
     /* プラグイン初期化 */
     canvas.clickableCanvas();
 
-    canvas.createShape('circle1', 'circle', {x: 80, y: 160, r: 50}, {
-      zindex: 15,
+    //元となる線の設定と描画
+    var l1 = canvas.createShape('line1', 'line', [
+        {x: 80, y: 160}, //始点
+        {x: 700, y: 160} //終点
+    ], {
+        style: {
+              lineWidth: 5,
+              fillStyle: '#555',
+            strokeStyle: '#555'
+        }
+    });
+
+    //元となる点の設定と描画
+     var c1 = canvas.createShape('circle1', 'circle', {x: 80, y: 160, r: 50}, {
       //図形のデフォルトのスタイル
       style: {
         strokeStyle: '#000',
@@ -47,14 +64,54 @@ $(function() {
       },
       //クリックしたときの処理
       onClick: function () {
-        this.animateShape('circle1', {
-          endcolor: {
-            strokeStyle: '#000',
-            fillStyle: '#000'
-          },
-          endlinewidth: 2,
-          endcoord: {x: 80, y: 160, r: 50}
-        });
+        cClicked1 = true;
+        dotClicked(c1, 'circle1');
       }
+
     });
+
+    //クリックされたら対象の要素の色を変更
+    //dotClicked(対象, 対象名)
+    function dotClicked(target, dotName) {
+
+      target.animateShape(dotName , {
+        endcolor: {
+          strokeStyle: '#000',
+          fillStyle: '#000'
+        },
+        endlinewidth: 2,
+        endcoord: {}
+      });
+      if(cClicked1 == true && cClicked2 == true){
+        dotLink();
+      }
+    }
+
+    //二点間を結ぶ線の描画
+    function dotLink(){
+
+      l1.animateShape('line1', {
+        endlinewidth: 5,
+        endcolor: {
+            strokeStyle: '#08F',
+              fillStyle: '#08F'
+        },
+        endcoord: [
+            {x: 80, y: 160},
+            {x: 700, y: 160}
+        ]
+      });
+    }
+
+    // circle1をコピー、circle2を描画
+    // canvas.copyShape(コピー元名, 新しい名前, {各種設定});
+    var c2 = canvas.copyShape('circle1', 'circle2', {
+        coords: {x: 700, y: 160, h: 80, w: 160, r: 50},
+        //クリックしたときの処理
+        onClick: function () {
+          cClicked2 = true;
+          dotClicked(c2, 'circle2');
+        }
+    });
+
 });
